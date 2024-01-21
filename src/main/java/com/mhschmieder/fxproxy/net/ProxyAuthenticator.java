eda0cdf9d5fdc49b308089b3ed02ac6c652506b6
Mark Schmieder <markhschmieder@gmail.com>
2024-01-20 22:04:16 -0500

    Missed an import statement in previous checkin

--- src/main/java/com/mhschmieder/fxproxy/net/ProxyAuthenticator.java
+++ src/main/java/com/mhschmieder/fxproxy/net/ProxyAuthenticator.java
@@ -1,7 +1,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2020, 2023 Mark Schmieder
+ * Copyright (c) 2020, 2024 Mark Schmieder
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,12 +21,12 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- * This file is part of the ProxyToolkit Library
+ * This file is part of the FxProxyGui Library
  *
  * You should have received a copy of the MIT License along with the
- * ProxyToolkit Library. If not, see <https://opensource.org/licenses/MIT>.
+ * FxProxyGui Library. If not, see <https://opensource.org/licenses/MIT>.
  *
- * Project: https://github.com/mhschmieder/proxytoolkit
+ * Project: https://github.com/mhschmieder/fxproxygui
  */
 package com.mhschmieder.fxproxy.net;
 
